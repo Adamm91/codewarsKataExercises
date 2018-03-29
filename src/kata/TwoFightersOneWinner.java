f3f887ec86b490c28438a6d327b58f27571cd5c2
Adamm91 <adam.marcinkowski91@gmail.com>
2018-03-29 14:42:21 +0200

    add Fighter class

--- src/kata/TwoFightersOneWinner.java
+++ src/kata/TwoFightersOneWinner.java
@@ -17,3 +17,13 @@ public class TwoFightersOneWinner {
         return null;
     }
 }
+
+class Fighter {
+    public String name;
+    public int health, damagePerAttack;
+    public Fighter(String name, int health, int damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+    }
+}
