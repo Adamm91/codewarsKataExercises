e8fd8911d2c27547c0aa14e0aea716495bba9576
Adamm91 <adam.marcinkowski91@gmail.com>
2018-03-29 18:11:40 +0200

    add CamelCase

---
+++ src/kata/CamelCase.java
@@ -0,0 +1,27 @@
+package kata;
+
+public class CamelCase {
+    // returns "theStealthWarrior"
+//    toCamelCase("the-stealth-warrior")
+
+    // returns "TheStealthWarrior"
+//    toCamelCase("The_Stealth_Warrior")
+    public static void main(String[] args) {
+        System.out.println(toCamelCase("the-stealth-warrior"));
+        System.out.println(toCamelCase("The_Stealth_Warrior"));
+    }
+
+    private static String toCamelCase(String input) {
+        String[] split = input.split("-|_| ");
+        for (int i = 1; i < split.length; i++) {
+            char[] array = split[i].toCharArray();
+            if (array[0] >= 'a' && array[0] <= 'z') {
+                array[0] -= 32;
+                split[i]= String.valueOf(array);
+            }
+        }
+        String camelCase = "";
+        for (String s : split) camelCase += s;
+        return camelCase;
+    }
+}
