87b22aa5115403d2d6042285b6fbda2e1c6eea66
Adamm1991 <adam.marcinkowski91@gmail.com>
2018-07-28 13:24:39 +0200

    add GrowthOfPopulation

--- src/kata/TimeFormatter.java
+++ src/kata/TimeFormatter.java
@@ -1,15 +1,15 @@
 package kata;
 
-import org.assertj.core.api.Assertions;
+//import org.assertj.core.api.Assertions;
 
 public class TimeFormatter {
-    public static void main(String[] args) {
-        Assertions.assertThat(format(62)).isEqualTo("1 minute and 2 seconds");
-        Assertions.assertThat(format(3662)).isEqualTo("1 hour, 1 minute and 2 seconds");
+//    public static void main(String[] args) {
+//        Assertions.assertThat(format(62)).isEqualTo("1 minute and 2 seconds");
+//        Assertions.assertThat(format(3662)).isEqualTo("1 hour, 1 minute and 2 seconds");
 
 //        System.out.println(format(62));
 //        System.out.println(format(3662));
-    }
+//    }
 
     private static String format(int seconds) {
         String time;
