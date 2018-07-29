230fc6eb6076d6316c9e5ce2e0b91b42888c8cc6
Adamm1991 <adam.marcinkowski91@gmail.com>
2018-07-29 20:56:00 +0200

    add SumOfPositive

---
+++ src/kata/SumOfPositive.java
@@ -0,0 +1,17 @@
+package kata;
+
+import java.util.Arrays;
+
+public class SumOfPositive {
+    /*
+    You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+     */
+
+    public static int sum(int[] arr){
+        return Arrays.stream(arr).map(a -> a > 0 ? a : 0).sum();
+    }
+}
