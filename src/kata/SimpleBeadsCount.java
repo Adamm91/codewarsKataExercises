a9ceccedc5546cf565dc4ef363a1a970b0997650
Adamm1991 <adam.marcinkowski91@gmail.com>
2018-08-14 20:06:50 +0200

    add description

--- src/kata/SimpleBeadsCount.java
+++ src/kata/SimpleBeadsCount.java
@@ -1,6 +1,14 @@
 package kata;
 
 public class SimpleBeadsCount {
+    /*
+    Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(N_blue) (in PHP count_red_beads($n); in Java, Javascript countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.
+     */
     public static int countRedBeads(final int nBlue) {
         return nBlue < 2 ? 0 : (nBlue - 1) * 2;
     }
