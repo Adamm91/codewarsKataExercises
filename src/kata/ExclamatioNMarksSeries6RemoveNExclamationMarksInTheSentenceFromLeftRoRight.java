7c90afaadfb8e28d5e704d658ec6a5918693e5ce
Adamm1991 <adam.marcinkowski91@gmail.com>
2018-12-26 18:59:13 +0430

    add ExclamatioNMarksSeries6RemoveNExclamationMarksInTheSentenceFromLeftRoRight

---
+++ src/kata/ExclamatioNMarksSeries6RemoveNExclamationMarksInTheSentenceFromLeftRoRight.java
@@ -0,0 +1,32 @@
+package kata;
+
+public class ExclamatioNMarksSeries6RemoveNExclamationMarksInTheSentenceFromLeftRoRight {
+    /*
+    Description:
+
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+     */
+
+    public static String remove(String s, int n) {
+        for (int i = 0; i < n; i++) s = s.replaceFirst("!", "");
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(remove("!!!Hi !!hi!!! !hi", 2));
+    }
+}
